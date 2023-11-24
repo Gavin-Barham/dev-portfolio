0ad241f83820c1e6886eb7bf7d269fe4f6944840
Gavin Barham <gavinbarham@yahoo.com>
2023-11-24 11:50:45 -0600

    attempt to fix spa 404 error for github pages with hashrouter instead of BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { BrowserRouter as Router} from 'react-router-dom'
+import { HashRouter as Router} from 'react-router-dom'
 import { Routes, Route } from "react-router-dom";
 
 import Header from './components/Header/Header'
